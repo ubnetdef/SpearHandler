0f7245c0d86463af0e7a8c1eeb28e43c42f35b2b
Blake <80996688+BlakeTnr@users.noreply.github.com>
2023-09-21 21:07:57 -0400

    Fix current tests & add async support

--- Operations/MythicClient.py
+++ Operations/MythicClient.py
@@ -11,7 +11,7 @@ class MythicClient(Client.Client):
     async def initializeMythic(self):
         # Todo: grab apitoken & ip from config
         apitoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQ0NDkwMzgsImlhdCI6MTY5NDQzNDYzOCwidXNlcl9pZCI6MSwiYXV0aCI6ImFwaSJ9.LvxS_fIGrc-W4xLswYRwAa3BbMCymoz0DJgddU666Yo"
-        mythicInstance = await mythic.login("34.237.94.238", apitoken=apitoken)
+        self.mythicInstance = await mythic.login("34.237.94.238", apitoken=apitoken)
 
     async def executeCommand(self, command, parameters, displayID):
         await mythic.issue_task_and_waitfor_task_output(self.mythicInstance, command, parameters, displayID)
--- Operations/operations_test.py
+++ Operations/operations_test.py
@@ -1,16 +1,18 @@
 from mythic import mythic
 import asyncio
 import MythicClient
+import pytest
 
+pytest_plugins = ('pytest_asyncio',)
+
+@pytest.mark.asyncio
 async def test_MythicClientCommand():
     mythicClient = MythicClient.MythicClient()
     await mythicClient.initializeMythic()
     mythicClient.displayId=7
     print(mythicClient)
     await mythicClient.executeCommand("ls", "", 7)
-    print("aaaaa")
     assert True
-asyncio.run(test_MythicClientCommand())
 
 def test_always_passes():
     assert True
