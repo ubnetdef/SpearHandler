200e89305f50a29267e8de1f884ea063ad0c05e9
Blake <80996688+BlakeTnr@users.noreply.github.com>
2023-09-24 15:00:54 -0400

    Operation (#4)
    * SyncingFromLaptop
    
    * Add file upload to MythicAgent
    
    ---------
    
    Co-authored-by: aryan-kum <93834917+aryan-kum@users.noreply.github.com>

--- Operations/MythicClient.py
+++ Operations/MythicClient.py
@@ -5,22 +5,30 @@ class MythicClient(Client.Client):
     mythicInstance = None
     displayID = None
 
-    def __init__(self):
-        return
+    def __init__(self, displayID):
+        self.displayID = displayID
 
     async def initializeMythic(self):
         # Todo: grab apitoken & ip from config
         apitoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQ0NDkwMzgsImlhdCI6MTY5NDQzNDYzOCwidXNlcl9pZCI6MSwiYXV0aCI6ImFwaSJ9.LvxS_fIGrc-W4xLswYRwAa3BbMCymoz0DJgddU666Yo"
         self.mythicInstance = await mythic.login("34.237.94.238", apitoken=apitoken)
 
-    async def executeCommand(self, command, parameters, displayID):
-        await mythic.issue_task_and_waitfor_task_output(self.mythicInstance, command, parameters, displayID)
+    async def executeShell(self, shellCommand):
+        return await mythic.issue_task_and_waitfor_task_output(self.mythicInstance, "shell", shellCommand, self.displayID)
 
-    # Agent downloads from mythic server
-    # Note:
-    # - Mythic agent must have LS capabilities (not shell command LS, but LS built into mythic) to see files using mythic built-in function
-    # (We could code our method via parsing shell ls output but ew)
-    # - LS command in mythic can NOT recursively list files, therefore must either know path or code our own recursion
-    async def uploadAndRunPayload(self, payload, runCommand):
-        raise NotImplementedError
-        # TODO
+    async def executeCommand(self, command, parameters):
+        return await mythic.issue_task_and_waitfor_task_output(self.mythicInstance, command, parameters, self.displayID)
+
+    async def getMythicInstance(self):
+        return self.mythicInstance
+
+    async def upload_file(self, fileBytes, remotePath):
+        fileID = await mythic.register_file(self.mythicInstance, "test.txt", fileBytes)
+
+        fileJSON = {
+            "host":"",
+            "remote_path":remotePath,
+            "file":fileID
+        }
+
+        await self.executeCommand("upload", fileJSON)
--- Operations/operations_test.py
+++ Operations/operations_test.py
@@ -7,12 +7,23 @@ pytest_plugins = ('pytest_asyncio',)
 
 @pytest.mark.asyncio
 async def test_MythicClientCommand():
-    mythicClient = MythicClient.MythicClient()
+    mythicClient = MythicClient.MythicClient(11)
     await mythicClient.initializeMythic()
-    mythicClient.displayId=7
     print(mythicClient)
     await mythicClient.executeCommand("ls", "", 7)
     assert True
+
+@pytest.mark.asyncio
+async def test_MythicClientUpload():
+    mythicClient = MythicClient.MythicClient(16)
+    await mythicClient.initializeMythic()
+    print(mythicClient)
+    await mythicClient.upload_file(b"test", "C:/Users/Blake/Desktop/test1")
+    await mythicClient.upload_file(b"test2", "C:/Users/Blake/Desktop/test2")
+
+    assert True
+
+asyncio.run(test_MythicClientUpload());
 
 def test_always_passes():
     assert True
