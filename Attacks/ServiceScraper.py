c59c31d7cdc0fe21a25a40800ea3edd5d1dd94f1
Blake <80996688+BlakeTnr@users.noreply.github.com>
2023-11-11 14:45:31 -0500

    Remove garbage files

--- Attacks/ServiceScraper.py
+++
@@ -1,8 +0,0 @@
-from Attack import Attack
-from Operations import Client
-import random
-import requests
-
-class ServiceScraper(Attack):
-    def execute(client: Client.Client):
-        client.executeShell("sudo lsof -i -n")
