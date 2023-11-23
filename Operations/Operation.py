d44fea6d970a979bdc89af1716b9ae9462fe0a49
BlakeTnr <blake@academiki.com>
2023-11-22 19:36:46 -0500

    Update to operation method

--- Operations/Operation.py
+++ Operations/Operation.py
@@ -3,7 +3,7 @@ from Attacks.RunNMAP import RunNMAP
 from Attacks.Attack import Attack
 from Data.Techniques.ClientsData import ClientsData
 from Data.Techniques.ClientData import *
-from Attacks.ServiceStopper import ServiceStopper
+from Attacks.ServiceStopper import ServiceStoppe
 
 class Operation():
     def __init__(self, inScopeIPs=[], clients=ClientsData()):
@@ -13,14 +13,30 @@ class Operation():
 
     def addAttack(self, attack: Attack):
         self.attackLibrary.append(attack)
+
+    def registerAllInScope(self):
+        clientsData = ClientsData()
+        for inScopeIP in self.inScopeIPs:
+            clientData = ClientData(inScopeIP)
+            clientsData.addClientData(clientData)
+        return clientsData
+
+    async def scanAll(self, attackClient: Client):
+        for clientData in self.clientsData.clients:
+            ip = clientData.ipAddress
+            # Working here
+            await attackClient.executeAttack(RunNMAP("nmaptest"), self)
+
     
     # A operation is intended to start w/ 1 client, that being a kali instance that will be the 'attacker', right off the bat it will then execute nmap
     async def startOperation(self):
         client: Client = self.c2Clients[0]
-        await client.executeAttack(RunNMAP("nmaptest"), self)
-        # Todo: add more to start of operation here
-        testUbuntu: Client = self.c2Clients[1]
-        await testUbuntu.executeAttack(ServiceStopper("servicestopped"), self)
+        await self.scanAll()
+        
+        # await client.executeAttack(RunNMAP("nmaptest"), self)
+        # # Todo: add more to start of operation here
+        # testUbuntu: Client = self.c2Clients[1]
+        # await testUbuntu.executeAttack(ServiceStopper("servicestopped"), self)
 
     async def runAttack(self, clientData: ClientData, attack: Attack):
         await clientData.c2Shell.executeAttack(attack)
